2aa88b964a3ecf4c50542f0ce02be3e1ee726500
ASWD13 <vibraniumrocker@gmail.com>
2024-09-22 14:18:35 +0530

    updated css colors

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./navbar.component.css";
 function Navbar() {
   return (
-    <nav class="navbar bg-dark border-bottom border-body">
+    <nav class="navbar">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">DOODLE AI</a>
     </div>
